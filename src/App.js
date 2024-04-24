006d78413f1bd7a94fe17f021ca9c9cde0c090fd
JDRiches <tothepowercore@gmail.com>
2024-04-24 19:08:28 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Dashboard from "./components/Dashboard";
 import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-200">
+    <div className="h-screen flex flex-col items-center justify-center bg-slate-200">
       <div className="flex flex-row items-start justify-center m-auto w-auto gap-1">
         <About />
         <Dashboard />
       </div>
+      <div className="flex flex-row">
+        <Footer />
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { FaLinkedin } from "react-icons/fa";
+import { ImGit, ImGithub } from "react-icons/im";
+
+
+function Footer() {
+  return (
+    <div className='flex flex-row h-10 w-full gap-5'>
+
+
+        <div className='flex flex-row gap-2 hover:underline'>
+            <FaLinkedin size={25}/> <a href='https://www.linkedin.com/in/jack-riches/'>jack-riches</a>
+        </div>
+
+        
+        <div className='flex flex-row gap-2 hover:underline'>
+            <ImGithub size={25}/> <a href='https://github.com/JDRiches'>@JDRiches</a>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Footer
