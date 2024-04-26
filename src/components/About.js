3b0ec652af8f1e7cd1f1202737e4d3b7c4322743
JDRiches <tothepowercore@gmail.com>
2024-04-26 17:23:29 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
  
     <div className='flex flex-col gap-5 my-10 w-full'>
-        <h1 className='text-4xl font-extrabold'>What's Powering Our Grid?</h1>
+        <h1 className='text-4xl font-extrabold'>What's Powering The Grid?</h1>
         <p className='font-medium'>This is a basic breakdown of the current fuel mix used for generating electricity supplied by the UK National Grid. Data is based on the current 30 minute period.</p>
         <p className='font-medium'>Use the drop down to view the generation mix for a specific region.</p>
         <p className='font-medium'>Carbon intensity data at a regional level is a forecast of the grams of CO2 per kWh for the 30 minute period.</p>
