33774ede65b92f9e75563ab90740c01623234be3
JDRiches <tothepowercore@gmail.com>
2024-11-20 09:27:17 +0000

    Update Region Selector

--- src/components/RegionSelector.js
+++ src/components/RegionSelector.js
@@ -1,39 +1,40 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from 'react';
 
-import axios from 'axios'
-
-function RegionSelector({ setRegion}) {
+function RegionSelector({ setRegion }) {
+    const regions = [
+        { value: "0", label: "National" },
+        { value: "1", label: "North Scotland" },
+        { value: "2", label: "South Scotland" },
+        { value: "3", label: "North West England" },
+        { value: "4", label: "North East England" },
+        { value: "5", label: "Yorkshire" },
+        { value: "6", label: "North Wales" },
+        { value: "7", label: "South Wales" },
+        { value: "8", label: "West Midlands" },
+        { value: "9", label: "East Midlands" },
+        { value: "10", label: "East England" },
+        { value: "11", label: "South West England" },
+        { value: "12", label: "South England" },
+        { value: "13", label: "London" },
+        { value: "14", label: "South East England" },
+        { value: "15", label: "England" },
+        { value: "16", label: "Scotland" },
+        { value: "17", label: "Wales" },
+    ];
 
     const handleRegionChange = (event) => {
         setRegion(event.target.value);
-        // You can perform any further actions with the selected value here
-      };
+    };
 
     return (
         <form className="">
-        <select onChange={handleRegionChange} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option value="0">National</option>
-            <option value="1">North Scotland</option>
-            <option value="2">South Scotland</option>
-            <option value="3">North West England</option>
-            <option value="4">North East England</option>
-            <option value="5">Yorkshire</option>
-            <option value="6">North Wales</option>
-            <option value="7">South Wales</option>
-            <option value="8">West Midlands</option>
-            <option value="9">East Midlands</option>
-            <option value="10">East England</option>
-            <option value="11">South West England</option>
-            <option value="12">South England</option>
-            <option value="13">London</option>
-            <option value="14">South East England</option>
-            <option value="15">England</option>
-            <option value="16">Scotland</option>
-            <option value="17">Wales</option>
-        </select>
+            <select onChange={handleRegionChange} id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                {regions.map((region) => (
+                    <option key={region.value} value={region.value}>{region.label}</option>
+                ))}
+            </select>
         </form>
-    )
+    );
 }
 
-export default RegionSelector
+export default RegionSelector;
